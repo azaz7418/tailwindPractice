05982588c51b5729a9ab55e4af99bdaca5831415
azaz7418 <wazaz7418@gmail.com>
2025-03-27 15:04:50 +0600

    Redux SetUp

--- src/Component/CountryProduct/CountryProduct.jsx
+++ src/Component/CountryProduct/CountryProduct.jsx
@@ -25,9 +25,10 @@ const CountryProduct = () => {
     <div className="countryProducts  pt-40 px-6 grid grid-cols-3 gap-6 ">
       {data?.map((cp) => (
         <div className=" " key={cp.idMeal}>
-          <div className=" w-96 h-96  text-orange-600 hover:bg-orange-500  hover:text-slate-300 hover:pt-10... shadow-lg rounded-md p-5 align-middle text-center ">
+          <div className=" w-96 h-96  text-zinc-700 shadow-lg bg-gray-200 rounded-md p-5 align-middle text-center ">
             <img className=" m-auto rounded-md w-80 h-60" src={cp.strMealThumb} alt="" />
             <h3 className=" text-xl mt-4 truncate">{cp.strMeal}</h3>
+            <button className="px-6 py-2 bg-blue-600 text-white font-semibold text-lg rounded-md">Add Cart</button>
           </div>
         </div>
       ))}
